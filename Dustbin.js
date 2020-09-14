85441d15932668c5ed1856d3bbbe4d984aa9afbc
Pooja7177 <69840048+Pooja7177@users.noreply.github.com>
2020-09-14 13:55:29 +0530

    Add files via upload

---
+++ Dustbin.js
@@ -0,0 +1,23 @@
+class Dustbin {
+    constructor (x,y,width,height){
+        var options  = {
+          isStatic: true }
+       this.width = width 
+       this.height  = height 
+       this.body  = Bodies.rectangle (x,y,width,height,options);   
+      this.image = loadImage("images/dustbingreen.png");
+       World.add(world,this.body);
+      
+
+  }
+  display(){
+      var pos =this.body.position;
+      push()
+      translate(pos.x,pos.y)
+      imageMode(CENTER);
+      fill("white");
+     rect(0, 0, this.width, this.height);
+     image(this.image,0, 0, this.width, this.height);
+      pop()
+  }
+}
